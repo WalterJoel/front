3f0428e52cc9fccfac4d547e5f6d39b2b2358878
WalterJoel <walter.valdivia@ucsp.edu.pe>
2022-10-21 09:16:04 -0500

    ss

--- kayogita/kayogafront/src/Pages/Home.js
+++ kayogita/kayogafront/src/Pages/Home.js
@@ -1,9 +1,12 @@
 import React from "react";
+import {Grid, Button} from '@mui/material';
+
 
 export default function Home(){
     return(
         <div>  
             <h1>Hola</h1>
+            <Button>Hola</Button>
         </div>
     )
 }
