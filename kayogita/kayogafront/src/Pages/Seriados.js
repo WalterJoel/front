aea219fb456ac8e5f8a5119bb5e8f497a52376ac
WalterJoel <walter.valdivia@ucsp.edu.pe>
2022-10-22 11:23:10 -0500

    Resolviendo el error de localhost

--- kayogita/kayogafront/src/Pages/Seriados.js
+++ kayogita/kayogafront/src/Pages/Seriados.js
@@ -62,7 +62,7 @@ const Seriados=()=> {
         alert(values);
        
         //For Production
-        fetch('backendkayoga-production.up.railway.app',{
+        fetch('http://backendkayoga-production.up.railway.app/postSeriados',{
         //For Develop
         //fetch('http://localhost:4000/postSeriados',{
             headers: {
